797f830bc15d07a05c83e24a0668f79870469186
Drilon Thaqi <ddrilonthaci@gmail.com>
2024-04-22 15:37:38 +0200

    Add VideoPost Like model

---
+++ VideoPortal.API/Models/Domain/VideoPostLike.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VideoPortal.API.Models.Domain
+{
+    public class VideoPostLike
+    {
+        public Guid Id { get; set; }
+        public Guid VideoPostId { get; set; }
+        public VideoPost VideoPost { get; set; }
+        public Guid UserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
